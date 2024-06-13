b12f5b465a0f544a69d4c6f0005d265168db3f07
Nguyen Tran Chung <nguyentranchung52th@gmail.com>
2024-06-13 23:08:08 +0900

    service

---
+++ docs/theme/components/OgImage.vue
@@ -0,0 +1,13 @@
+<template>
+  <img
+    :src="`https://banners.beyondco.de/${encodeURIComponent(
+      name
+    )}?theme=light&packageManager=&packageName=&pattern=ticTacToe&style=style_1&description=&md=1&showWatermark=0&fontSize=100px&images=lightning-bolt`"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+</script>
--- docs/theme/index.ts
+++ docs/theme/index.ts
@@ -1,8 +1,12 @@
 // docs/.vitepress/theme/index.ts
 import DefaultTheme from "vitepress/theme";
 import Layout from "./Layout.vue";
+import type { Theme } from "vitepress";
 
 export default {
   extends: DefaultTheme,
   Layout,
-};
+  enhanceApp({ app }) {
+    // register your custom global components
+  },
+} satisfies Theme;
